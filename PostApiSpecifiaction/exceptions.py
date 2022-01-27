76331fe7b410cd506f66501d819c83e365cdb9eb
Matei-L <lipanmatei@gmail.com>
2022-01-27 02:03:08 +0200

    postAPI cleanup

--- PostApiSpecifiaction/exceptions.py
+++
@@ -1,5 +0,0 @@
-class PreconditionFailedException(Exception):
-
-    def __init__(self, message=""):
-        self.message = "412: Precondition Failed: " + message
-        super().__init__(self.message)
--- PostApiSpecifiaction/extractor.py
+++
@@ -1,8 +0,0 @@
-from validator import validate
-
-
-def extract(request):
-    body = request.get_json()
-    validate(body)
-
-    return body['id'], body['url'], body['open_api_specification']
